e0ba80efe72e14ca8c1b0b332cf12bc4de31e8df
killex8569 <alexandreanette@gmail.com>
2025-10-03 09:55:27 +0200

    last commit on bataille_naval bash for the moment

--- Bataille naval (bash)/bataille_naval.sh
+++ Bataille naval (bash)/bataille_naval.sh
@@ -15,5 +15,21 @@
 #                 Aussi, Le dernier joueurs à mettre en place ces bateaux est le premier à jouer (pareils, sinon il pourrais voir les bateaux de l'adversaire)
 
 
+# Création d'une grille de 1 à 10 et de A à J
 
+taille_grille=10
+letters=(A B C D E F G H I J)
+grille_de_jeux=($(for ((i=0; i<taille_grille*taille_grille; i++)); do echo "_"; done))
+
+
+for ((i=0; i<${#grille_de_jeux[@]}; i++)); do
+    if (( (i) % taille_grille == 0)); then
+        echo # Saut de ligne entre chaque ligne
+        printf "%s" "${letters[i/taille_grille]}"
+        printf " " # Espace entre Lettre et Grille
+    fi
+    printf "%s" "${grille_de_jeux[$i]}"
+    printf " "
+done
+echo
 
--- test.sh
+++ test.sh
@@ -2,22 +2,30 @@
 
 # Tableau à plusieurs valeurs
 
-tableau=("1 2 3" "4 5 6" "7 8 9") # Tableau en bash
+tableau=("_ _ _" "_ _ _" "_ _ _") # Tableau en bash
 
 for tableau in "${tableau[@]}"; do
+    tableau="_ * _"
     echo "$tableau"
 done
 
 # Création d'une grille de 1 à 10 et de A à J
 
 taille_grille=10
+letters=(A B C D E F G H I J)
+grille_de_jeux=("1 2 3" "4 5" "")
+# grille_de_jeux=($(for ((i=0; i<taille_grille*taille_grille; i++)); do echo "_"; done))
 
-grille_de_jeux=($(for ((i=0; i<taille_grille*taille_grille; i++)); do echo "_"; done))
+#for ((i=0; i<${#grille_de_jeux[@]}; i++)); do
+#    if (( i % taille_grille == 0)); then # Verifie si le nb est divisable par 10
+#        echo # Saut de ligne entre chaque ligne
+#        printf "%s" "${letters[i/taille_grille]}"
+#        printf " " # Espace entre Lettre et Grille
+#    fi
+#    printf "%s" "${grille_de_jeux[$i]}"
+#    printf " "
+#done
 
 
-for ((i=0; i<${#grille_de_jeux[@]}; i++)); do
-    echo "${grille_de_jeux[$i]}"
-    if (( (i+1) % taille_grille == 0)); then
-        echo;
-    fi
-done
+echo
+
