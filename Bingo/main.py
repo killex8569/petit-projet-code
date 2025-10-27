6a5c9131719ebfd49425adc23d774cd2d44ea909
killex8569 <alexandreanette@gmail.com>
2025-10-27 12:25:09 +0100

    Feat : Function random number in list python

--- Bingo/main.py
+++ Bingo/main.py
@@ -1,5 +1,26 @@
 import random
 
+taille_grille = 15
+
 bingo_1_j = []
 bingo_2_j = []
 
+bingo_1_o = []
+bingo_2_o = []
+
+nb_tirer = []
+
+# Tirage de la manière suivante : 
+# 15 nombre entre 1 et 89
+
+def create_grille(list_tableau):
+    print("create_grille")
+    i = 0
+    while i <= taille_grille:
+        nb = random.randrange(90)
+        list_tableau.append(nb)
+        i = i + 1
+    print(list_tableau)
+
+create_grille(bingo_1_j)
+print("test : ", bingo_1_j)
